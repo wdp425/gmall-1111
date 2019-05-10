66eaac8320909a2f958aa6459715ba2380cd5607
leifengyang <534096094@qq.com>
2019-05-10 08:50:57 +0800

    项目逆向完成....

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,24 @@
 package com.atguigu.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.DigestUtils;
 
+
+/**
+ * 1、配置整合dubbo
+ * 2、配置整合MyBatisPlus
+ *
+ * logstash整合，
+ * 1）、导jar
+ * 2）、导日志配置
+ * 3）、在kibana里面建立好日志的索引，就可以可视化检索
+ *
+ */
+@EnableDubbo
+@MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
